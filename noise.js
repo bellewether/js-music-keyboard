55a4cd79243ecf1c0adbfd69d8cb974146954915
Yasmin <yasminor@gmail.com>
2016-11-21 20:55:03 -0800

    plays a note when clicked

--- noise.js
+++ noise.js
@@ -1,3 +1,12 @@
 $(document).ready( function() {
-  // your code here
+
+  var playNote = function(letter) {
+    var audio = $("#" + letter + "Audio")[0];
+    audio.play();
+  };
+
+  $('.instrument').on('click', 'button', function(event) {
+    var note = $(this).html();
+    playNote(note);
+  });
 });
